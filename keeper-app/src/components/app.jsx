3158067690fc5584bdf647096fe0fbdcb1fa208a
Vaibhav Agarwal <89684455+vaibhav-ag1@users.noreply.github.com>
2022-04-11 20:26:44 +0530

    Add files via upload

---
+++ keeper-app/src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Node from "./node";
+function App()
+{
+    return <div>
+        <Header/>
+        <Node/>
+        <Footer/>
+    </div>
+}
+
+export default App;
+
---
+++ keeper-app/src/components/node.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Node()
+{
+    return <div className="note">
+        <h1>This is Heading</h1>
+        <p> This is paragraph</p>
+    </div>
+}
+
+export default Node;
